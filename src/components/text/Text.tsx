11cb958ee706969c1c5fc053b0f1d4da476ba484
donaldturinglee <donaldturinglee@gmail.com>
2025-12-08 07:10:52 -0800

    feat: Add Text component with polymorphic behavior, size and weight variants, and corresponding styles and tests

---
+++ src/components/text/Text.tsx
@@ -0,0 +1,33 @@
+import React, { useRef, type ForwardedRef } from "react";
+import { ClassNames } from "../../utilities/classnames";
+import { TextProps } from "./Text.types";
+import "./Text.scss";
+import { useForwardedRef } from "../../hooks/useForwardedRef";
+import { PolymorphicForwardRef } from "../../utilities/polymorphic";
+
+function TextBase<As extends React.ElementType = "span">(
+    props: TextProps<As>,
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    ref: ForwardedRef<any>,
+) {
+    const { as: Component = "span", size, weight, className, ...rest } = props;
+
+    const textRef = useRef<HTMLElement>(null);
+    useForwardedRef<HTMLElement>(ref, textRef);
+
+    const classes = ClassNames("tofu-text", className);
+
+    return (
+        <Component
+            ref={textRef}
+            data-size={size}
+            data-weight={weight}
+            className={classes}
+            {...rest}
+        />
+    );
+}
+
+TextBase.displayName = "Text";
+
+export const Text = PolymorphicForwardRef(TextBase);
