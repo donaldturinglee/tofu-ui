e33dcdf5772248acaeea25e4f0f14d2d9141c6e7
carlo <awiezzz@qq.com>
2026-01-16 21:39:17 +0800

    feat:Remove SCSS dependency

--- tests/jest.config.js
+++ tests/jest.config.js
@@ -3,7 +3,7 @@ module.exports = {
     testEnvironment: "jsdom",
     setupFilesAfterEnv: ["<rootDir>/tests/jest.setup.js"],
     moduleNameMapper: {
-        "\\.(css|less|scss|sass)$": "identity-obj-proxy",
+        "\\.css$": "identity-obj-proxy",
     },
     transform: {
         "^.+\\.(ts|tsx|js|jsx)$": "babel-jest",
