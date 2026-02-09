9dc8b5311bbe4b1ba514380d76bbb6ea5ef0c1a7
carlo <awiezzz@qq.com>
2026-02-10 00:03:17 +0800

    fix：Update button styling code

--- src/components/button/Button.types.ts
+++ src/components/button/Button.types.ts
@@ -1,30 +1,30 @@
-import React, { ComponentPropsWithoutRef, ElementType, Ref } from "react";
+import React, { ComponentPropsWithoutRef, ElementType, Ref, ReactNode } from "react";
 
 export type ButtonShape = "square" | "round" | "circle";
-export type ButtonVariant = "default" | "primary" | "solid" | "soft" | "surface" | "outline" | "ghost";
+export type ButtonVariant =
+    | "default"
+    | "primary"
+    | "solid"
+    | "soft"
+    | "surface"
+    | "outline"
+    | "ghost";
 export type ButtonIntent = "success" | "warning" | "danger" | "info";
 export type ButtonSize = "small" | "medium" | "large";
 
-export interface BaseButtonProps {
+export interface ButtonProps extends Omit<ComponentPropsWithoutRef<"button">, "type" | "color"> {
     as?: ElementType;
     highContrast?: boolean;
     variant?: ButtonVariant;
     intent?: ButtonIntent;
     size?: ButtonSize;
     disabled?: boolean;
-    isLoading?: boolean;
+    loading?: boolean;
     block?: boolean;
     shape?: ButtonShape;
     inactive?: boolean;
-    children?: React.ReactNode;
-    leadingVisual?: React.ReactNode;
-    trailingVisual?: React.ReactNode;
-}
-
-export interface ButtonProps extends BaseButtonProps, Omit<
-    ComponentPropsWithoutRef<"button">,
-    "type" | "color"
-> {
+    children?: ReactNode;
+    leadingVisual?: ReactNode;
+    trailingVisual?: ReactNode;
     ref?: Ref<HTMLButtonElement>;
 }
-
